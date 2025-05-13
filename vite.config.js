4ff8b46d43cf0e8f181e62cebd4208d1cbc96b64
Carlos Vidal <carlos@mitienda.pe>
2025-05-13 15:56:46 -0500

    Mejorar configuración de redirección para rutas anidadas en el CMS

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     'process.env': {
       VITE_API_URL:
         process.env.NODE_ENV === 'production'
-          ? 'https://your-render-api-url.onrender.com'
+          ? 'https://taita-api.onrender.com'
           : 'http://localhost:3000',
     },
   },
